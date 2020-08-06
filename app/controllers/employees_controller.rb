c3bd785e40357d50c6e1d6da9686b282391d6271
CodyC <codyicastro@gmail.com>
2020-08-06 16:09:43 -0400

    Initial Commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,34 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:edit, :show]
+    
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+    end
+
+    def new 
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to employee_path(@employee)
+    end 
+
+    def edit 
+    end 
+
+    private 
+
+    def employee_params 
+        params.require(:employee).permit! 
+    end
+
+    def find_employee
+        @employee = Employee.all.find(params[:id])
+    end
+
+
 end
